834c4d89a8ba5fcf9c016baa698a1a9c246d446e
Yuvraj Singh <yuvi0393@gmail.com>
2015-07-01 19:55:13 +0530

    Create Plugins to test embed module

---
+++ test/modules/embedtype_annoatation_entity_test/src/Plugin/EmbedType/EmbedCategoryEmbed.php
@@ -0,0 +1,18 @@
+<?php
+
+ /**
+  * @file
+  * contains \Drupal\embedtype_annoatation_entity_test\Plugin\EmbedType\EmbedCategoryEntity
+  */
+
+  namespace \Drupal\embedtype_annoatation_entity_test\Plugin\EmbedType;
+
+ /**
+  * Defines the embedcategoryentity plugin
+  *
+  * @EmbedCategoryEntity(
+  *   category = "entity"
+  * )
+  */
+  class EmbedCategoryEntity {
+  }
---
+++ test/modules/embedtype_annoatation_url1_test/src/Plugin/EmbedType/EmbedCategoryUrl1.php
@@ -0,0 +1,18 @@
+<?php
+
+ /**
+  * @file
+  * contains \Drupal\embedtype_annoatation_url1_test\Plugin\EmbedType\EmbedCategoryUrl1
+  */
+
+  namespace \Drupal\embedtype_annoatation_url1_test\Plugin\EmbedType;
+
+ /**
+  * Defines the "embedcategoryurl1" plugin
+  *
+  * @EmbedCategoryEntity(
+  *   category = "url"
+  * )
+  */
+  class EmbedCategoryUrl1 {
+  }
