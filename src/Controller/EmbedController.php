5c2f6f8ef74bc77081cc0293d79a93b61df590b6
Dave Reid <dave@davereid.net>
2015-08-05 15:33:11 -0500

    Added Preview controller and embed.routing.yml.

---
+++ src/Controller/EmbedController.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\embed\Controller\EmbedController.
+ */
+
+namespace Drupal\embed\Controller;
+
+use Drupal\Core\Ajax\AjaxResponse;
+use Drupal\Core\Ajax\ReplaceCommand;
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\filter\FilterFormatInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+/**
+ * Returns responses for Embed module routes.
+ */
+class EmbedController extends ControllerBase {
+
+  /**
+   * Returns an Ajax response to generate preview of an entity.
+   *
+   * Expects the the HTML element as GET parameter.
+   *
+   * @param \Symfony\Component\HttpFoundation\Request $request
+   *   The request object.
+   * @param \Drupal\filter\FilterFormatInterface $filter_format
+   *   The filter format.
+   *
+   * @throws \Symfony\Component\HttpKernel\Exception\NotFoundHttpException
+   *   Throws an exception if 'text' parameter is not found in the request.
+   *
+   * @return \Symfony\Component\HttpFoundation\Response
+   *   The preview of the entity specified by the data attributes.
+   */
+  public function preview(Request $request, FilterFormatInterface $filter_format) {
+    $text = $request->get('value');
+    if ($text == '') {
+      throw new NotFoundHttpException();
+    }
+
+    $entity_output = (string) check_markup($text, $filter_format->id());
+
+    $response = new AjaxResponse();
+    $response->addCommand(new ReplaceCommand(NULL, $entity_output));
+    return $response;
+  }
+
+}
