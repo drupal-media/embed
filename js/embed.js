90b253735819685863bd85b530806d9cd3145d86
wimleers <wimleers@99777.no-reply.drupal.org>
2015-12-06 18:40:36 +0100

    Issue #2624462 by Wim Leers: Clean up embed.js: missing parameter documentation, JS coding standards violations.

--- js/embed.js
+++ js/embed.js
@@ -1,43 +1,52 @@
 /**
  * @file
- * Drupal Entity plugin.
+ * Drupal Embed module.
  */
 
 (function ($, Drupal) {
 
-  "use strict";
+  'use strict';
 
   /**
    * Attaches or detaches behaviors, except the ones we do not want.
    *
    * @param {string} action
    *   Either 'attach' or 'detach'.
-   * @param context
+   * @param {HTMLDocument|HTMLElement} context
    *   The context argument for Drupal.attachBehaviors()/detachBehaviors().
-   * @param settings
+   * @param {object} settings
    *   The settings argument for Drupal.attachBehaviors()/detachBehaviors().
    */
-  Drupal.runEmbedBehaviors = function(action, context, settings) {
+  Drupal.runEmbedBehaviors = function (action, context, settings) {
     // Do not run the excluded behaviors.
     var stashed = {};
     $.each(Drupal.embed.excludedBehaviors, function (i, behavior) {
-        stashed[behavior] = Drupal.behaviors[behavior];
-        delete Drupal.behaviors[behavior];
+      stashed[behavior] = Drupal.behaviors[behavior];
+      delete Drupal.behaviors[behavior];
     });
     // Run the remaining behaviors.
-    (action == 'attach' ? Drupal.attachBehaviors : Drupal.detachBehaviors)(context, settings);
+    (action === 'attach' ? Drupal.attachBehaviors : Drupal.detachBehaviors)(context, settings);
     // Put the stashed behaviors back in.
     $.extend(Drupal.behaviors, stashed);
-  }
+  };
 
   /**
    * Ajax 'embed_insert' command: insert the rendered embedded item.
    *
    * The regular Drupal.ajax.commands.insert() command cannot target elements
-   * within iframes. This is a skimmed down equivalent that works whether the
-   * CKEditor is in iframe or div area mode.
+   * within iframes. This is a skimmed down equivalent that works no matter
+   * whether the CKEditor is in iframe or div area mode.
+   *
+   * @param {Drupal.Ajax} ajax
+   *   An Ajax object.
+   * @param {object} response
+   *   The Ajax response.
+   * @param {string} response.data
+   *    The Ajax response's content.
+   * @param {number} [status]
+   *   The HTTP status code.
    */
-  Drupal.AjaxCommands.prototype.embed_insert = function(ajax, response, status) {
+  Drupal.AjaxCommands.prototype.embed_insert = function (ajax, response, status) {
     var $target = ajax.element;
     // No need to detach behaviors here, the widget is created fresh each time.
     $target.html(response.data);
@@ -48,6 +57,7 @@
    * Stores settings specific to Embed module.
    */
   Drupal.embed = {
+
     /**
      * A list of behaviors which are to be excluded while attaching/detaching.
      *
@@ -55,6 +65,7 @@
      * - Drupal.behaviors.contextual, to keep contextual links hidden.
      */
     excludedBehaviors: ['editor', 'contextual']
+
   };
 
 
